146eedee55a6fc147c2cf952c90e9cc757936c1f
Jeff <jeffersonvsoares@hotmail.com>
2026-06-26 15:00:04 -0300

    fix: compose preview annotation
    just to testing shared release

--- shared/src/commonMain/kotlin/com/workflow/shared/App.kt
+++ shared/src/commonMain/kotlin/com/workflow/shared/App.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun App() {
@@ -15,4 +16,10 @@ fun App() {
     ) {
         Text("Hello from Shared Module!")
     }
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    App()
 }
