69b1a2441f4b888dc58dd42e7104bf03711f5687
Jeff <jeffersonvsoares@hotmail.com>
2026-06-27 22:00:55 -0300

    feat: test shared release with notes

--- shared/src/commonMain/kotlin/com/workflow/shared/App.kt
+++ shared/src/commonMain/kotlin/com/workflow/shared/App.kt
@@ -1,24 +1,29 @@
 package com.workflow.shared
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun App(modifier: Modifier = Modifier) {
+fun App(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(16.dp),
+) {
     Box(
-        modifier = modifier,
+        modifier = modifier.padding(contentPadding),
         contentAlignment = Alignment.Center
     ) {
         Text("Hello from Shared Module!")
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun AppPreview() {
     App()
